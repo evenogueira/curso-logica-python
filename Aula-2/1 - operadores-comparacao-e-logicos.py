260b2960a71fb7240eafa089c162d3bb39c5aedc
Eveline Nogueira <eve@MacBook-Air-de-Eveline.local>
2023-03-09 22:03:38 -0300

    aula 2

---
+++ Aula-2/1 - operadores-comparacao-e-logicos.py
@@ -0,0 +1,24 @@
+x = 5
+y = 15
+z = 15
+a = 10 
+#valores booleanos True ou False
+#operadores comparacao
+# == igual à
+# != diferente de
+# < menor que
+# > maior que
+
+#resultado = x != y
+#resultado2 = z == y
+#print(resultado2)
+
+#operadores logicos
+#and
+#print(5 < 10 and 1 > 10)
+
+#or
+#print(5 > 10 or 1 > 10)
+
+#not
+print(not(10 < 5))
