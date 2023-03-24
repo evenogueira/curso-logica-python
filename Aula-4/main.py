a0c8e68a5032486d28b97e165bc7c2a856d69d6a
Eveline Nogueira <eve@MacBook-Air-de-Eveline.local>
2023-03-24 01:37:01 -0300

    aula-4

--- Aula-4/main.py
+++ Aula-4/main.py
@@ -2,33 +2,31 @@
 
 #while
 
-# contador = 0
+contador = 0
 
-# while contador <= 5:
-#     print(contador)
-#     contador += 1
+while contador <= 5:
+    print(contador)
+    contador += 1
 
 
 #for
-
-# for i in range(5):
-#     print(i)
+for i in range(5):
+    print(i)
 
 #iteracao listas e tuplas
-
 lista = ["casa","cadeira","carro","cachorro"]
 
-# for i in lista:
-    # print(i)
+for i in lista:
+    print(i)
 
 #usando o break
+for i in lista:
+    if i == "carro":
+        break
+    print(i)
 
-# for i in lista:
-#     if i == "carro":
-#         break
-#     print(i)
+
 #usando o continue
-
 for i in lista:
     if i == "carro":
         continue
